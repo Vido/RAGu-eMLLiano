8890ebaf21685bd1d9bdca993e0437b83b6c3370
Lucas Vido <956388+Vido@users.noreply.github.com>
2024-10-11 01:48:12 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,68 @@
 import os
-from flask import Flask, request
 
+from flask import Flask, request
 app = Flask(__name__)
+
+# Generative AI Config
+from config_creator import create_aicore_config
+create_aicore_config()
+
+from gen_ai_hub.proxy.langchain.openai import ChatOpenAI
+from gen_ai_hub.proxy.core.proxy_clients import get_proxy_client
+
+proxy_client = get_proxy_client('gen-ai-hub')
+LLM = ChatOpenAI(proxy_model_name='gpt-35-turbo', proxy_client=proxy_client)
 
 @app.route('/')
 def hello():
     return 'RAGu eMLLiano'
 
+def embeddings():
+    from hdbcli import dbapi
+    from langchain_community.vectorstores.hanavector import HanaDB
+    from gen_ai_hub.proxy.langchain.init_models import init_embedding_model
+
+    embeddings = init_embedding_model('text-embedding-ada-002')
+    connection = dbapi.connect(
+        os.getenv("SAP_HANA_HOST"),
+        port="443",
+        user=os.getenv("SAP_HANA_USER"),
+        password=os.getenv("SAP_HANA_PASS"),
+        autocommit=True,
+        sslValidateCertificate=False,
+    )
+    db = HanaDB(embedding=embeddings, connection=connection,
+        table_name="CATALOG_UPDATED_DEV_1_"+ hdb_user)
+
+    return db
+
+def get_prompt():
+    from langchain.prompts.chat import
+    # TODO
+    prompt_template = "You are a helpful AI bot. "
+        "Context: {context} Question: {question}"
+    return PromptTemplate(
+        template = prompt_template, 
+        input_variables=["context", "question"])
+
 @app.route('/chat', methods=['GET'])
 def chat():
-	q = request.args.get('q')
-    return q
+    question = request.args.get('q')
+
+    db = embeddings()
+    retriever = db.as_retriever(search_kwargs={'k':20})
+
+    from langchain.chains import RetrievalQA
+    qa = RetrievalQA.from_chain_type(llm=LLM,
+                     retriever=retriever, 
+                     chain_type="stuff",
+                     chain_type_kwargs={"prompt": get_prompt()})
+
+    answer = qa.run(question)
+    return answer
 
 if __name__ == '__main__':
     # Port number is required to fetch from env variable
     # http://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html#PORT
-    CF_PORT = os.getenv("PORT", default=8000)
-    app.run(host='0.0.0.0', port=int(CF_PORT), debug=True)
+    cf_port = os.getenv("PORT", default=8000)
+    app.run(host='0.0.0.0', port=int(cf_port), debug=True)
